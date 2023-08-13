d93ee4eeaf19032a137a3f8aed9510dc9da369b6
maher-cshub <maherarab.1299@gmail.com>
2023-08-13 14:37:58 +0100

    before redirect user and admin

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,12 @@
-class AdminController < ApplicationController
+class UserController < ApplicationController
 
   protect_from_forgery with: :exception
 
-  before_action :authenticate_admin!
+  before_action :authenticate_user!
 
   protected
 
   def after_sign_in_path_for(resource)
     current_user.is_a?(Admin) ? dashboard2_index_path: (stored_location_for(resource) || root_path)
   end
-
-
-
 end
